811540a6c87281b7be3dbedcae4f526a9d39bea5
alagie singhateh <3939919@gmail.com>
2021-12-27 01:11:30 +0700

    Programming with singhateh Laravel Car Parking Management System Version 1

--- resources/views/vehicles/create.blade.php
+++ resources/views/vehicles/create.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-md-12">
         <div class="card">
-            <div class="card-header"><h3>Create Adminstrator</h3></div>
+            <div class="card-header"><h3>Create Vehicle </h3></div>
             <div class="card-body">
               @include('vehicles.fields')
             </div>
